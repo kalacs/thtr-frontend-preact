70012c3e642f0dba8ea2bcca14d9086f406ef954
Zoltan Kakuk <kakuk.zoltan@e-vista.hu>
2020-05-19 22:50:56 +0200

    Change config

--- server.js
+++ server.js
@@ -1,16 +1,4 @@
 const makeFrontendApp = require("./index");
-
-const appConfig = {
-  frontend: {
-    scraperUrl: "http://192.168.0.124:3000/scraper",
-    torrentsUrl: "http://192.168.0.124:3000/torrents",
-    movieAPIUrl: "https://api.themoviedb.org/3/",
-    movieAPIKey: "9f1ffd64abd4bde18614fd9087d87d71",
-  },
-  backend: {
-    port: 3002,
-    host: "192.168.0.124",
-  },
-};
+const appConfig = require("./config.json");
 const frontendApp = makeFrontendApp(appConfig);
 frontendApp.start();
