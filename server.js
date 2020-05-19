4b3c20c11da40d344d61734911448266d315b4a4
Zoltan Kakuk <kakuk.zoltan@e-vista.hu>
2020-05-19 12:31:08 +0200

    Make module

--- server.js
+++ server.js
@@ -1,7 +1,5 @@
-const micro = require("micro");
-const { send } = micro;
-const handler = require("serve-handler");
-const { router, get } = require("microrouter");
+const makeFrontendApp = require("./index");
+
 const appConfig = {
   frontend: {
     scraperUrl: "http://192.168.0.124:3000/scraper",
@@ -10,27 +8,9 @@ const appConfig = {
     movieAPIKey: "9f1ffd64abd4bde18614fd9087d87d71",
   },
   backend: {
-    port: 3001,
+    port: 3002,
     host: "192.168.0.124",
   },
 };
-
-const getConfig = (req, res) => send(res, 200, appConfig.frontend);
-const staticHandler = async (request, response) => {
-  return await handler(request, response, {
-    public: `${__dirname}/build`,
-  });
-};
-
-const routes = router(get("/config", getConfig), get("/*", staticHandler));
-const server = micro(routes);
-
-server.listen(appConfig.backend.port, appConfig.backend.host, (err) => {
-  if (err) {
-    console.log(err);
-    process.exit(1);
-  }
-  console.log(
-    `Serving frontend... @${appConfig.backend.host}:${appConfig.backend.port}`
-  );
-});
+const frontendApp = makeFrontendApp(appConfig);
+frontendApp.start();
