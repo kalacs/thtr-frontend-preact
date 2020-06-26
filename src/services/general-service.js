e3515a3a6fe67b0224986a09ec6aee237da457dc
Zoltan Kakuk <kakuk.zoltan@e-vista.hu>
2020-06-26 18:23:37 +0200

    Remove console.log

--- src/services/general-service.js
+++ src/services/general-service.js
@@ -1,5 +1,4 @@
 export const getConfig = () => {
-  console.log(`${window.location.origin}/config`);
   return fetch(`${window.location.origin}/config`).then((response) =>
     response.json()
   );
